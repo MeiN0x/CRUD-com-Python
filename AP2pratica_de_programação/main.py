f3c3a1d9d6e633e14f1866c8f943e1c3a9d303f0
Mei <yasminsgof@gmail.com>
2021-05-29 18:48:18 -0300

    Att

--- AP2pratica_de_programação/main.py
+++ AP2pratica_de_programação/main.py
@@ -17,12 +17,13 @@ while opcao != 6:
      \33[1:33m2\33[m - \33[0:34mCadastrar Motos\33[m
      \33[1:33m3\33[m - \33[0:34mComprar Motos\33[m
      \33[1:33m4\33[m - \33[0:34mConsultar Vendas\33[m
-     \33[1:33m5\33[m - \33[0:34mDeletar Moto\33[m
+     \33[1:33m5\33[m - \33[0:34mEditar Cadastro\33[m
      \33[1:33m6\33[m - \33[0:34mSair\33[m
      ''')
 # Validando inputs ---------------------------------------------
     try:
-        opcao = int(input('     O que deseja fazer? '))
+        print(linha*2)
+        opcao = (int(input(('\33[1:33m                                   O que deseja fazer? \33[m'))))
     except:
         print('Opção inválida. Tente novamente.')
         sleep(1)
@@ -32,9 +33,10 @@ while opcao != 6:
     # ------------------------------------------Cadastrando Clientes------------------------------------------------
     if opcao == 1:
         while True:
-            novoCliente = (str(input('Qual o seu nome? ')))
+            novoCliente = (str(input('     Qual o seu nome? ')))
+            CPFcliente = (str(input('     Qual o seu CPF? (XXX.XXX.XXX-XX) ')))
             with open('cadastro_clientes.txt', 'a') as cadastro_clientes:
-                cadastro_clientes.write(novoCliente + '\n')
+                cadastro_clientes.write( f'|  Cliente: {novoCliente}  |  CPF: {CPFcliente}  \n ')
             resp = str(input('Deseja cadastrar mais algum cliente?[S/N]'))
             if resp in 'nN':
                 break
@@ -53,9 +55,14 @@ while opcao != 6:
             resp = str(input('Quer adicionar mais motos? [S/N] '))
             if resp in 'nN':
                 break
-# Deletando motos -------------------------------------------------------------------------------------------------
-    elif opcao == 5:
+
+# Comprando motos -------------------------------------------------------------------------------------------------
+    elif opcao == 3:
         while True:
+            print(linha)
+            with open('cadastro_motos.txt', 'r') as cadastro_motos:
+                for moto in cadastro_motos:
+                    print(f'{moto}', end='')
             print(linha)
             placa = str(input('Digite a placa do veículo (Formato LLL-NNNN): ')).upper().strip()
             if len(placa) == 8 and '-' in placa:
@@ -70,23 +77,29 @@ while opcao != 6:
                 if placa in moto:
                     print('Veículo selecionado:')
                     print(moto)
-                    confirm = str(input('Deseja remover o veículo?(S/N)\n'))
+                    confirm = str(input('Deseja comprar o veículo?(S/N)\n'))
                     if confirm in 'sS':
+                        with open ('lista_vendas.txt','w+') as lista_vendas:
+                            salvar = lista_vendas.write(moto)
                         texto.remove(moto)
                         cadastro_motos = open("cadastro_motos.txt", "w")
                         cadastro_motos.writelines(texto)
                         cadastro_motos.close()
-                        print('\033[1;32mRemovendo Veículo...\033[m')
+                        print('\033[1;32mComprando veículo...\033[m')
                         sleep(1.5)
-                        print('\033[1;32mVeículo removido com Sucesso\033[m')
+                        print('\033[1;32mVeículo comprado com sucesso\033[m')
                         break
             else:
                 if confirm in 'sS':
-                    print('\n\033[1;31mEsta placa não foi encontrada ou já foi removida. Tente novamente.\033[m')
+                    print('\n\033[1;31mEsta placa não foi encontrada ou já foi comprada. Tente novamente.\033[m')
+
 # Mostrando Motos ---------------------------------------------------------------------------------------------------
     elif opcao == 4:
-        with open('cadastro_motos.txt', 'r') as cadastro_motos:
-            for moto in cadastro_motos:
+        print(linha * 2)
+        print("{:^100}".format('\33[1:33mListagem de vendas\33[m'))
+        print(linha * 2)
+        with open('lista_vendas.txt', 'r') as lista_vendas:
+            for moto in lista_vendas:
                 print(f'{moto}', end='')
         print()
         print(linha*2)
