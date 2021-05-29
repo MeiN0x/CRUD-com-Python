254195263e908c59ecbe9774c6769147573ab0d6
Mei <yasminsgof@gmail.com>
2021-05-28 21:40:57 -0300

    Update main.py

--- AP2pratica_de_programação/main.py
+++ AP2pratica_de_programação/main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+linha = '~' * 50
+titulo = '\33[1:35mCadastro de vendas\33[m'
+menu = '\33[1:33mMenu principal\33[m'
+print(linha)
+print(titulo.center(50))
+print(linha)
+print(menu.center(50))
+print(linha)
+opção = 0
+while opção != 5:
+    print('''   
+     \33[1:33m1\33[m - \33[0:34mCadastrar Clientes\33[m
+     \33[1:33m2\33[m - \33[0:34mCadastrar Motos\33[m
+     \33[1:33m3\33[m - \33[0:34mComprar Motos\33[m
+     \33[1:33m4\33[m - \33[0:34mConsultar Vendas\33[m
+     \33[1:33m5\33[m - \33[0:34mSair\33[m
+     ''')
+    opção = int(input('     O que deseja fazer? '))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    if opção == 1:
+        clientes = []
+        while True:
+            clientes.append(str(input('Qual o seu nome? ')))
+            resp = str(input('Deseja cadastrar mais algum cliente?[S/N]'))
+            if resp in 'nN':
+                break
+    elif opção == 2:
+        motos = []
+        while True:
+            motos.append(str(input('Marca: ')))
+            motos.append(str(input('Modelo: ')))
+            motos.append(int(input('Ano: ')))
+            motos.append(float(input('Preço: ')))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+            resp = str(input('Quer adicionar mais motos? [S/N] '))
+            if resp in 'nN':
+                break
